fdd1d5c1aa4d73961ab505d17a6a4ca88ae4c1ea
Issi <alex.morosow1@gmail.com>
2020-10-20 20:04:43 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"device-reminder adapter settings": {
+		"en": "Adapter settings for device-reminder",
+		"de": "Adaptereinstellungen für device-reminder",
+		"ru": "Настройки адаптера для device-reminder",
+		"pt": "Configurações do adaptador para device-reminder",
+		"nl": "Adapterinstellingen voor device-reminder",
+		"fr": "Paramètres d'adaptateur pour device-reminder",
+		"it": "Impostazioni dell'adattatore per device-reminder",
+		"es": "Ajustes del adaptador para device-reminder",
+		"pl": "Ustawienia adaptera dla device-reminder",
+		"zh-cn": "device-reminder的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
