cc2a154b6429647e0a223cfee7db129d626571c4
Xenon-s <65488191+Xenon-s@users.noreply.github.com>
2021-05-05 20:47:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -71,7 +71,11 @@ class deviceReminder extends utils.Adapter {
 
         // get language from system
         const strTemp = await this.getForeignObjectAsync('system.config');
-        lang = strTemp.common !== undefined ? strTemp.common.language || 'en' : 'en';
+        if (strTemp !== undefined) {
+           lang = strTemp.common.language;
+        } else {
+            lang = 'en';
+        };
 
         this.log.warn(statesMsg.costs[lang])
 
